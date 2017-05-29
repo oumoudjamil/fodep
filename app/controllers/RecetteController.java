de3ea9cd2268136e856428b89b32b5616745993a
Oumou <djamil.dev@gmail.com>
2017-05-29 12:44:13 +0000

    Correction swagger last

--- app/controllers/RecetteController.java
+++ app/controllers/RecetteController.java
@@ -15,6 +15,7 @@ import io.swagger.annotations.*;
 /**
  * Created by djamil on 27/05/2017.
  */
+@Api(value = "/lesposts", description = "Renvoie les posts")
 public class RecetteController extends Controller {
 
     @ApiOperation(
@@ -30,7 +31,6 @@ public class RecetteController extends Controller {
             @ApiResponse(code = 450, message = "Parametres incorrects.", response = Recette.class),
             @ApiResponse(code = 451, message = "Donnees erronees.", response = Recette.class),
     })
-
     public Result getAllRecette() throws SQLException {
         ServiceRecetteImpl serviceRecette = new ServiceRecetteImpl();
         ArrayList<Recette> recettes = serviceRecette.getAllRecette();
@@ -38,7 +38,7 @@ public class RecetteController extends Controller {
         ObjectNode objectNode = Json.newObject();
 
         objectNode.put("result", "ok");
-        objectNode.put("code", "1000");
+        objectNode.put("code", "200");
         objectNode.put("message", "");
         objectNode.put("recettes", Json.toJson(recettes));
         Logger.debug("recettes " + Json.toJson(recettes).toString());
