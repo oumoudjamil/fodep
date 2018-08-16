e5ee9f87c78f38d01e7b0587ee94a1fb28f82352
ndeya <ndeye.awa2017@hotmail.com>
2018-08-16 15:53:13 +0000

    correct add recipe

--- public/javascripts/recette.js
+++ public/javascripts/recette.js
@@ -145,7 +145,7 @@ $(document)
        function verifyBeforeDoCreateRecette(){
 
             var name = $('#tfadd_name').val();
-            var photo = $('#fileLocation').val();
+            var photo = url0;
             var duration = $('#tfadd_duration').val();
             var categorie = $('#seladd_categorie').val();
             var information = $('#tfadd_description').val();
