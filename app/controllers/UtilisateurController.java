2e83d5c915d2dabc1da234d35571133b81d30b73
ndeya <ndeye.awa2017@hotmail.com>
2018-08-30 09:51:51 +0000

    connexion page

--- app/controllers/UtilisateurController.java
+++ app/controllers/UtilisateurController.java
@@ -39,8 +39,17 @@ public class UtilisateurController {
             }
             UtilisateurDAO dao = DAOFactory.getUtilisateurDAO();
             ArrayList<Utilisateur> getUser = dao.connectUser(login, mdp);
+            Logger.info("User  " + Json.toJson(getUser));
             if(!getUser.isEmpty()) {
-            user.put("result", "ok");
+
+                    play.mvc.Controller.ctx().session().clear();
+                    play.mvc.Controller.ctx().session().put(Const.SESSION_CONNECTED, "true");
+                    play.mvc.Controller.ctx().session().put(Const.SESSION_LOGIN, login);
+                    play.mvc.Controller.ctx().session().put(Const.SESSION_ID_USER, UtilisateurDaoImpl.iduser);
+                    play.mvc.Controller.ctx().session().put(Const.SESSION_NOM, UtilisateurDaoImpl.nom);
+                    play.mvc.Controller.ctx().session().put(Const.SESSION_PRENOM, UtilisateurDaoImpl.prenom);
+
+                user.put("result", "ok");
                 user.put("code", "3000");
                 user.put("message", "");
                 user.put("users", Json.toJson(getUser));
